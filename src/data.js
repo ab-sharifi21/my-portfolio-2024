211b41d30edb251f371ce69d6dde98abc2647e19
Abdullah Sharifi <ab.sharifi19@gmail.com>
2023-10-25 00:09:54 +0200

    change the p2p project description in data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ const projects = [
     {
         id: 1,
         name: 'P2P - Marketplace',
-        description: 'Buying and selling platform for second hand video games, consoles, accessories and PCs. The users can search for products by category, price, name and location. It allows users to sing up for achieving complete access so that they can add and buy products...',
+        description: 'Buying and selling platform for second hand video games, consoles, accessories and PCs. The users can search for products by their category, price, name and location. It allows users to sing up for achieving complete access so that they can add and buy products...',
         github: 'https://github.com/ab-sharifi21/Player2Player-Plataforma-Compra-Venta',
         url: '',
         tools: [
