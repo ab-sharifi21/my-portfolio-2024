4fc9b509fe64ef956296ff9cc232079991a359ba
Abdullah Sharifi <ab.sharifi19@gmail.com>
2024-02-10 21:06:03 +0100

    Add new skills (TypeScript, Next.js, Docker) to the skills section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,11 +1,11 @@
 import { IoLogoHtml5 } from 'react-icons/io';
 import { DiCss3 } from 'react-icons/di';
 import { IoLogoJavascript } from 'react-icons/io';
-import { FaReact } from 'react-icons/fa';
+import { FaDocker, FaReact } from 'react-icons/fa';
 import { BiLogoGit } from 'react-icons/bi';
 import { BsGithub } from 'react-icons/bs';
 import { IoLogoNodejs } from 'react-icons/io';
-import { SiMysql } from 'react-icons/si';
+import { SiMysql, SiNextdotjs, SiTypescript } from 'react-icons/si';
 import Working from '../../assets/working.svg';
 import './skills.css';
 
@@ -19,11 +19,14 @@ const Skills = () => {
                     <li><IoLogoHtml5 /> HMTL</li>
                     <li><DiCss3 /> CSS</li>
                     <li><IoLogoJavascript /> JavaScript</li>
-                    <li><FaReact /> React</li>
+                    <li><SiTypescript /> TypeScript</li>
+                    <li><FaReact /> React.js </li>
+                    <li><SiNextdotjs /> Next.js</li>
                     <li><BiLogoGit /> Git</li>
                     <li><BsGithub /> Github</li>
                     <li><IoLogoNodejs /> Node</li>
                     <li><SiMysql /> SQL</li>
+                    <li><FaDocker /> Docker </li>
                 </ul>
             </div>
         </section>
